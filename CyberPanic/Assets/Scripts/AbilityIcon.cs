8840da8600353b3e83e1354142517e01b37313de
Nate-HomeDesk <colednate@gmail.com>
2019-05-23 16:30:59 -0700

    Adding Ability, AbilityIcon, and Character scripts

---
+++ CyberPanic/Assets/Scripts/AbilityIcon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityIcon : MonoBehaviour
+{
+
+	public string abilityName;
+	public int SPCost;
+	public int statModifier;
+   
+}
